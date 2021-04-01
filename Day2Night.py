7725dc0602a6e28b3616ee9a9c7615cfb1b80133
Ayan Khan <programing.ninja0@gmail.com>
2021-04-02 01:22:08 +0530

    Add files via upload

---
+++ Day2Night.py
@@ -0,0 +1,27 @@
+import numpy as np
+from imageio import imread,imwrite
+import cv2
+
+#Night Effect
+
+img = imread('day.jpg')
+
+arr = img * np.array([0.1, 0.2, 0.5])
+
+arr2 = (255*arr/arr.max()).astype(np.uint8)
+
+imwrite('night.png',arr2)
+
+img2 = cv2.imread('night.png')
+
+gamma = 2 # adjust accordingly more the gamma more will be the darkness
+
+gamma_img = np.array(255*(img2/255)**gamma,dtype = 'uint8')
+
+cv2.imwrite('night_final.png',gamma_img)
+
+print("Conversion Done!")
+
+
+# Set Files Names Accordingly in Same Folder.
+
